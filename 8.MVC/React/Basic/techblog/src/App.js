88200118a26d9fe901cdf5e4b8bcee1243c50680
Daniel UndreamtMayhem Marcial Johnson <danny_boyx@hotmail.co.uk>
2019-04-27 00:16:34 +0100

    added route

--- 8.MVC/React/Basic/techblog/src/App.js
+++ 8.MVC/React/Basic/techblog/src/App.js
@@ -1,87 +1,109 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import Sidebar from './Components/Sidebar';
+import Home from './Components/Home';
+import AddPost from './Components/AddPost';
+import Post from './Components/Post';
+import Footer from './Components/Footer';
 import './App.css';
 
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+// footer broken styles
+
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [
-        {
-          'title': 'The Impact the Software Industury has on Trees',
-          'author': 'efjeiruoewiur',
-          'commentcount': 2,
-          'datecreated': new Date().getDate,
-          'img': 'https://placeimg.com/640/480/any',
-          'shorttext': `Nam tempor turpis et urna sagittis pharetra. Nullam molestie lorem non arcu venenatis,
-                        non imperdiet neque venenatis.Praesent sed dignissim velit.Nam vel lorem velit.Aliquam eget neque pretium,
-                        placerat magna at,
-                        consectetur arcu.In ac feugiat quam.Fusce consequat pellentesque lacinia.Class aptent taciti sociosqu ad litora torquent per conubia nostra,
-                        per inceptos himenaeos.Nullam fermentum,
-                        lacus sed aliquam auctor,
-                        dui diam porta ipsum,
-                        eget luctus elit est sit amet magna`
-        }, {
-          'title': 'The Impact the Software Industury has on Trees',
-          'author': 'efjeiruoewiur',
-          'commentcount': 2,
-          'datecreated': new Date().getDate,
-          'img': 'https://placeimg.com/640/480/any',
-          'shorttext': `Nam tempor turpis et urna sagittis pharetra. Nullam molestie lorem non arcu venenatis,
-                        non imperdiet neque venenatis.Praesent sed dignissim velit.Nam vel lorem velit.Aliquam eget neque pretium,
-                        placerat magna at,
-                        consectetur arcu.In ac feugiat quam.Fusce consequat pellentesque lacinia.Class aptent taciti sociosqu ad litora torquent per conubia nostra,
-                        per inceptos himenaeos.Nullam fermentum,
-                        lacus sed aliquam auctor,
-                        dui diam porta ipsum,
-                        eget luctus elit est sit amet magna`
-        }
-      ]
-    };
-  }
 
   render() {
 
-    let articles;
-    articles = this
-      .state
-      .articles
-      .map((item, index) => {
-        console.log(item.title);
-
-        return (
-          <article>
-            <header>
-              <h2>{item.title}</h2>
-              <div class="meta">
-                Posted By
-                <strong>{item.author}1 Day Ago</strong>
-                <span>({item.commentcount}
-                  Comments)</span>
-              </div>
-            </header>
-            <img src={item.img} alt="Post 1"/>
-            <p>{item.shorttext}</p>
-            <div class="ad">
-              <img src="img/ad.jpg" alt="Advertisement"/>
-            </div>
-            <footer>
-              <a href="#single-post.html" class="black-button">
-                <i class="fa fa-paper-plane w3-margin-right"></i>Read More</a>
-            </footer>
-          </article>
-        );
-      })
-
     return (
-      <section >
-        {articles}
-        <div class="clearfix"></div>
-      </section>
+      <div>
 
-    );
+        <Sidebar/>
 
+        <Router>
+          <header id="main-header">
+            <div className="container">
+              <div id="branding">
+                <img src="../img/log.png" alt="The DailiesTech"/>
+                <h1 className="title">DailiesTech</h1>
+              </div>
+              <nav id="main-nav">
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/add/">Add</Link>
+                  </li>
+                  <li>
+                    <Link to="/post/">Post</Link>
+                  </li>
+                </ul>
+                <i
+                  onClick={this.handleClick}
+                  styles="color: white;"
+                  className="fa fa-bars"
+                  aria-hidden="true"
+                  id="hamburger-btn"></i>
+              </nav>
+            </div>
+          </header>
+
+          <main class="container">
+            <aside id="sidebar" class="">
+              <div class="search">
+                <form>
+                  <input type="search" placeholder="Search Our Blog..."/><input type="image" src="./img/search.png" alt="Search"/>
+                </form>
+              </div>
+              <div class="clearfix"></div>
+              <nav id="sub-nav">
+                <ul>
+                  <li>
+                    <a href="/">Web Development</a>
+                  </li>
+                  <li>
+                    <a href="/">Design</a>
+                  </li>
+                  <li>
+                    <a href="/">SEO & Marketing</a>
+                  </li>
+                  <li>
+                    <a href="/">Cloud & Hosting</a>
+                  </li>
+                  <li>
+                    <a href="/">Gadgets</a>
+                  </li>
+                  <li>
+                    <a href="/">Software</a>
+                  </li>
+                </ul>
+              </nav>
+              <div class="ad">
+                <img src="./img/ad.jpg" alt="Advertisement"/>
+              </div>
+            </aside>
+
+            <Route path="/" exact component={Home}/>
+            <Route path="/add/" component={AddPost}/>
+            <Route path="/post/" component={Post}/>
+
+          </main>
+
+        </Router>
+
+        <Footer/>
+      </div>
+
+    )
   }
+
 }
 
 export default App;
